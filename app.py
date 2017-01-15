081a40a0e8b0ad1c636b076004e3a822a4a1b9f0
terry <terry@SPECTRE16>
2017-01-15 19:41:11 +0000

    linked to ic2pro site

--- app.py
+++ app.py
@@ -1,39 +1,10 @@
-"""
-This script runs the application using a development server.
-"""
+from bottle import route, run, template
+import requests
 
-import bottle
-import os
-import sys
+@route('/temp')
+def index():
+    #using http://www.instructables.com/id/Temperature-Monitor-with-ESP8266-IoT/
+    r = requests.get(r'https://www.ic2pro.com/Wire/connector/get?id=930d6623-21f4-4d61-affd-2f370202145d&TEMPERATURE', auth=('terryspitz@gmail.com','temperature'))
+    return r.text +"C"
 
-# routes contains the HTTP handlers for our server and must be imported.
-import routes
-
-if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
-    # Debug mode will enable more verbose output in the console window.
-    # It must be set at the beginning of the script.
-    bottle.debug(True)
-
-def wsgi_app():
-    """Returns the application to make available through wfastcgi. This is used
-    when the site is published to Microsoft Azure."""
-    return bottle.default_app()
-
-if __name__ == '__main__':
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-
-    @bottle.route('/static/<filepath:path>')
-    def server_static(filepath):
-        """Handler for static files, used with the development server.
-        When running under a production server such as IIS or Apache,
-        the server should be configured to serve the static files."""
-        return bottle.static_file(filepath, root=STATIC_ROOT)
-
-    # Starts a local test server.
-    bottle.run(server='wsgiref', host=HOST, port=PORT)
+run(host='localhost', port=8080)
